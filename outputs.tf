83f907d0a181d1f99b845c61b8e0bbdf66e12d6a
Nicolas Venne <68240042+nickvenne@users.noreply.github.com>
2022-02-22 09:59:42 -0500

    feat: add arn output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "cdn_domain_name" {
   description = "Domain name of the Cloudfront Distribution"
   value       = "${aws_cloudfront_distribution.cdn.domain_name}"
 }
+output "cdn_arn" {
+  description = "ARN of the Cloudfront Distribution"
+  value       = "${aws_cloudfront_distribution.cdn.arn}"
+}
